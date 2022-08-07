c3b3688dde05a195eab8a7298f7ca28e0c8864fd
Nikita <nikitaperepelov040604@gmail.com>
2022-08-07 17:59:53 +0300

    Orbit Movement added

--- Assets/Scripts/OrbitMovement.cs
+++ Assets/Scripts/OrbitMovement.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class OrbitMovement : MonoBehaviour
 {
-    public Transform Center;
+    public Transform Planet;
 
     public float Radius;
     public float RadiusVelovity;
     public float RotationVelocity;
 
     private Vector3 axis;
-    private Vector3 DesiredPosition;
 
     void Start()
     {
-        transform.position = (transform.position - Center.position).normalized * Radius + Center.position;
         axis = Vector3.forward;
     }
 
     void Update()
     {
-        transform.RotateAround(Center.position, axis, RotationVelocity * Time.deltaTime);
-        DesiredPosition = (transform.position - Center.position).normalized * Radius + Center.position;
-        transform.position = Vector3.MoveTowards(transform.position, DesiredPosition, RadiusVelovity * Time.deltaTime);
+        transform.RotateAround(Planet.position, axis, RotationVelocity * Time.deltaTime);
+        transform.position = (transform.position - Planet.position).normalized * Radius;
+        if (Radius < 1)
+        {
+            Radius = 1;
+        }
     }
 }

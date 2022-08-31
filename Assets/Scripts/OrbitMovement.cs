15f1bdce43228d5e8671438018e68a9740dc3fcc
Nikita <nikitaperepelov040604@gmail.com>
2022-08-31 20:11:46 +0300

    Relation with object and planet added

--- Assets/Scripts/OrbitMovement.cs
+++ Assets/Scripts/OrbitMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -9,13 +10,17 @@ public class OrbitMovement : MonoBehaviour
     public float Velocity;
     public float Radius;
 
+    public Transform Planet;
+
     void FixedUpdate()
     {
-        float x = Mathf.Sin(2 * Mathf.PI * Time.fixedTime * Velocity) * Radius;
-        float y = Mathf.Cos(2 * Mathf.PI * Time.fixedTime * Velocity) * Radius;
+        float x = Mathf.Sin(2 * Mathf.PI * Time.fixedTime * Velocity) * Radius + Planet.position.x;
+        float y = Mathf.Cos(2 * Mathf.PI * Time.fixedTime * Velocity) * Radius + Planet.position.y;
         transform.position = new Vector3(x, y, 0f);
 
-        float angle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.AngleAxis(angle + 20, Vector3.forward);
+        Vector3 relatedPosition = Planet.position - transform.position;
+
+        float angle = Mathf.Atan2(relatedPosition.y, relatedPosition.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }

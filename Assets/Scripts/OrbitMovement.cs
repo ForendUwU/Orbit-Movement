242611882c52458ab38281f62e00176a4460e15b
Nikita <nikitaperepelov040604@gmail.com>
2022-09-04 17:58:12 +0300

    Rotation without "magic" numbers

--- Assets/Scripts/OrbitMovement.cs
+++ Assets/Scripts/OrbitMovement.cs
@@ -18,7 +18,7 @@ public class OrbitMovement : MonoBehaviour
         float y = Mathf.Cos(2 * Mathf.PI * Time.fixedTime * Velocity) * Radius + Planet.position.y;
         transform.position = new Vector3(x, y, 0f);
 
-        Vector3 relatedPosition = transform.position - Planet.position ;
+        Vector3 relatedPosition = transform.position - Planet.position;
 
         float angle = Mathf.Atan2(relatedPosition.x, relatedPosition.y) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.AngleAxis(-angle, Vector3.forward);
